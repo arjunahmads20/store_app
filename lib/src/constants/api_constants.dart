7059b88a1e4cd1a21390d71cc7fa025a8bdfe7d1
arjunahmads20 <arjunahmads20@gmail.com>
2026-05-01 13:11:10 +0700

    Change base URL from HTTP to HTTPS

--- lib/src/constants/api_constants.dart
+++ lib/src/constants/api_constants.dart
@@ -4,7 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:store_app/src/features/auth/data/token_storage.dart';
 
 class ApiConstants {
-  static const String baseUrl = 'http://storeappxyz.pythonanywhere.com/api/v1'; // Be carefull to distinct http with https
+  static const String baseUrl = 'https://storeappxyz.pythonanywhere.com/api/v1'; // Be carefull to distinct http with https
   static const String products = '/product/products/';
   static const String orders = '/order/orders/';
   static const String deliveryTypes = '/order/delivery-types/';
@@ -19,7 +19,7 @@ class ApiConstants {
 // Network Provider
 final dioProvider = Provider<Dio>((ref) {
   final dio = Dio(BaseOptions(
-    baseUrl: 'http://storeappxyz.pythonanywhere.com/api/v1', // Use 10.0.2.2 for Android Emulator if needed
+    baseUrl: 'https://storeappxyz.pythonanywhere.com/api/v1', // Use 10.0.2.2 for Android Emulator if needed
     connectTimeout: const Duration(seconds: 120),
     receiveTimeout: const Duration(seconds: 120),
     headers: {
